b72b0378d7466b3d2ec6883e104388360516bebc
william <william.sereno@hotmail.com>
2021-11-16 12:46:35 -0300

    criado e removido atibuto dinâmico de objetos do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,3 +17,7 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Sereno'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(william.__dict__)
